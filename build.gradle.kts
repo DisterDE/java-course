cfdff90a5f95d1a977a4a7ded5f854f6281846af
DisterRU <disterko@gmail.com>
2021-08-04 02:05:59 +0300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.5.21"
+}
+
+group = "com.kachalov.gu.android"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
